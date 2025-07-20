c3cce52f819232e27ca22c7333c6e2f8a50f2cd6
Kor Dwarshuis <proximus@fastmail.com>
2025-07-20 14:07:47 +0200

    Set base path in Vite configuration to use VITE_BASE_PATH environment variable

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const basePath = process.env.VITE_BASE_PATH || '/';
+
 export default defineConfig({
+  base: basePath,
   plugins: [vue()],
   server: {
     port: 3000,
