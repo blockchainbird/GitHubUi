c6166afbb517d8f89414d4593c739c12df2483b7
Kor Dwarshuis <proximus@fastmail.com>
2025-07-20 16:39:13 +0200

    Refactor Vite configuration to load environment variables for base path

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-const basePath = process.env.VITE_BASE_PATH || '/';
-
-export default defineConfig({
-  base: basePath,
-  plugins: [vue()],
-  server: {
-    port: 3000,
-    open: true
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    base: env.VITE_BASE_PATH || '/',
+    plugins: [vue()],
+    server: {
+      port: 3000,
+      open: true
+    }
   }
 })
