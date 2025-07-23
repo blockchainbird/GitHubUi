6e309eaf748b006013fdd69b2170acf66a044bf4
Kor Dwarshuis <proximus@fastmail.com>
2025-07-23 10:20:06 +0200

    fix: Add build configuration to disable code splitting in Rollup

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,14 @@ export default defineConfig(({ mode }) => {
     server: {
       port: 3000,
       open: true
+    },
+    // sets the Rollup output option manualChunks to undefined during the build process. This disables code splitting for dynamic imports, so all code will be bundled into a single output file instead of splitting into multiple chunks. This can be useful if you want a single JavaScript file for deployment, but it may increase the bundle size and affect loading performance for larger apps.
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: undefined
+        }
+      }
     }
   }
 })
