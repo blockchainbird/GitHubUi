79642ad541fc9189fe8cbd604e22bacf66e0d753
Kor Dwarshuis <proximus@fastmail.com>
2025-08-13 20:28:18 +0200

    feat: Remove commented-out div for repository info in RepoInfo component

--- src/components/RepoInfo.vue
+++ src/components/RepoInfo.vue
@@ -1,11 +1,8 @@
 <template>
-    <!-- Repository Info Row -->
-    <!-- <div class=" "> -->
-        <i class="bi bi-github me-2"></i>
-        <code class="bg-light px-2 py-1 rounded border">{{ owner }}/{{ repo }}</code>
-        <span class="mx-2">•</span>
-        <span class="badge bg-secondary">{{ branch }}</span>
-    <!-- </div> -->
+  <i class="bi bi-github me-2"></i>
+  <code class="bg-light px-2 py-1 rounded border">{{ owner }}/{{ repo }}</code>
+  <span class="mx-2">•</span>
+  <span class="badge bg-secondary">{{ branch }}</span>
 </template>
 
 <script>
