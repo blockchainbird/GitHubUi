e48ec00dfe73654a36cd61d675c7d8201fc56fd0
Kor Dwarshuis <proximus@fastmail.com>
2025-08-13 20:45:16 +0200

    feat: Wrap RepoInfo component content in a div for improved structure

--- src/components/RepoInfo.vue
+++ src/components/RepoInfo.vue
@@ -1,8 +1,10 @@
 <template>
-  <i class="bi bi-github me-2"></i>
-  <code class="bg-light px-2 py-1 rounded border">{{ owner }}/{{ repo }}</code>
-  <span class="mx-2">•</span>
-  <span class="badge bg-secondary">{{ branch }}</span>
+  <div>
+    <i class="bi bi-github me-2"></i>
+    <code class="bg-light px-2 py-1 rounded border">{{ owner }}/{{ repo }}</code>
+    <span class="mx-2">•</span>
+    <span class="badge bg-secondary">{{ branch }}</span>
+  </div>
 </template>
 
 <script>
